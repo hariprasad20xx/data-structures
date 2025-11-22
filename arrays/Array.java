f7b3799428b4c9f3a44e3bf4eefe12cb1ac3aad1
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-22 22:37:11 +0530

    Create Scanner object

--- arrays/Array.java
+++ arrays/Array.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 public class Array {
     
     public static void main(String[] args) {
-        
+
+        Scanner scanner = new Scanner (System.in);
+
+        scanner.close();        
     }
 }
