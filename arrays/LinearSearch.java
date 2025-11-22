bc577b8df1438d680a8597e882798eae6a0e8916
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-22 22:43:45 +0530

    Create Scanner object

--- arrays/LinearSearch.java
+++ arrays/LinearSearch.java
@@ -1,8 +1,12 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class LinearSearch {
     
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        scanner.close();
     }
 }
