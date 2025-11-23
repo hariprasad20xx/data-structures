329ba5f3b2b1bc8bc6881c92dd53dbd3c6d2d038
Hariprasad <hariprasadjnv963@gmail.com>
2025-11-23 21:56:02 +0530

    Handle user input

--- arrays/LinearSearch.java
+++ arrays/LinearSearch.java
@@ -7,7 +7,16 @@ public class LinearSearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int[] arr = new int[10];
+        System.out.print("Enter the number of elements: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");  
+        for (int i = 1; i <=n ; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
 
         scanner.close();
     }
