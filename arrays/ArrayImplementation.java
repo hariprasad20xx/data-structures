b078ae15ebbe93110b75c92a6ea31ea3f2381f8a
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-03 22:12:31 +0530

    Import Scanner class

--- arrays/ArrayImplementation.java
+++ arrays/ArrayImplementation.java
@@ -1,9 +1,11 @@
 package arrays;
 
+import java.util.Scanner;
+
 public class ArrayImplementation {
     // Program to implement array 
 
     public static void main(String[] args) {
-        
+
     }
 }
