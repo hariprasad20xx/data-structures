6ea265b030f40f9863dd09dac8c0c1ef96720674
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-03 22:14:07 +0530

    Create Scanner object

--- arrays/ArrayImplementation.java
+++ arrays/ArrayImplementation.java
@@ -6,6 +6,8 @@ public class ArrayImplementation {
     // Program to implement array 
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
+        scanner.close();
     }
 }
