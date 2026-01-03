6598b00205be6d541a651ab3099e0eaccc005953
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-03 22:16:28 +0530

    Remove main method

--- arrays/Array.java
+++ arrays/Array.java
@@ -4,10 +4,5 @@ import java.util.Scanner;
 
 public class Array {
     
-    public static void main(String[] args) {
-
-        Scanner scanner = new Scanner (System.in);
-
-        scanner.close();        
-    }
+    
 }
