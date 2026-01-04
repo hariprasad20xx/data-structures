b4be4041d90fe40aab7e6a0af5c8d8af54efa2a6
Hariprasad <hariprasadjnv963@gmail.com>
2026-01-04 22:00:13 +0530

    Handle user input

--- arrays/BinarySearch.java
+++ arrays/BinarySearch.java
@@ -7,6 +7,19 @@ public class BinarySearch {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        System.out.print("Enter the size of the array: ");
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.print("Enter " + n + " elements for the array: ");  
+        for (int i = 0; i < n ; i++) {
+            arr[i] = scanner.nextInt();
+        }
+
+        System.out.print("Enter the element to be searched: ");
+        int key = scanner.nextInt();
+
         scanner.close();
     }
 }
